708bb8f654dc14d7657f1c429e9c64352746e4ab
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-02-14 21:13:26 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,12 @@
+INPUT_SCHEMA = {
+  "message": 
+    {
+      "example": [
+        "How to count all the stars in the night sky?"
+      ],
+      "shape": [
+        1
+      ],
+      "datatype": "BYTES"
+    }
+}
